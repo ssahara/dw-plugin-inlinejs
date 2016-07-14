be1f3040e7a3ac811a15499327db7699ecceba2e
Satoshi Sahara <sahara.satoshi@gmail.com>
2016-07-14 20:42:59 +0900

    remove getMethds() in helper
    it seems unnecessary?

--- helper.php
+++ helper.php
@@ -12,33 +12,15 @@ if(!defined('DOKU_INC')) die();
 class helper_plugin_inlinejs extends DokuWiki_Plugin {
 
     /**
-     * Returns some documentation of the methods provided by this helper part
-     *
-     * @return array Method description
-     */
-    function getMethods() {
-        $result = array();
-        $result[] = array(
-                'name'   => 'renderInlineJsHtml',
-                'desc'   => 'render HTML of inline JavaScript',
-                'params' => array(
-                        'renderer'=>'renderer',
-                        'script' => 'string'),
-                'return' => array('html' => 'string'),
-        );
-        return $result;
-    }
-
-    /**
      * render HTML of inline JavaScript
      */
     function renderInlineJsHtml(Doku_Renderer $renderer, $script) {
         if (empty($script)) return false;
 
-        $html = '<script type="text/javascript">'.NL.'/*<![CDATA[*/'.NL;
-        $html.= $script;
-        $html.= '/*!]]>*/'.NL.'</script>'.NL;
-        $renderer->doc.=$html;
+        $html  = '<script type="text/javascript">'.DOKU_LF.'/*<![CDATA[*/'.DOKU_LF;
+        $html .= $script;
+        $html .= '/*!]]>*/'.DOKU_LF.'</script>'.DOKU_LF;
+        $renderer->doc .= $html;
         return true;
     }
 
